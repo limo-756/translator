0ffcb44d3073887a90d4132ac88f1a8ffffd2915
limo-756 <anuragsharma05031996@gmail.com>
2017-04-21 15:33:22 +0530

    Add ruleSelector method to Rule class.

--- translation.java
+++ translation.java
@@ -37,4 +37,50 @@ class Rules
         }
         fp.fileClose();
     }
+    /**
+     * it selects the rule
+     * @param rule contaion part of speech of words in source sentence
+     */
+    void ruleSelector(String rule)
+    {
+        int arr[]= new int[maxRules];
+        HashMap<String,Integer> mapPartOfSpeech=new HashMap<String,Integer>();
+        FileRead fp=new FileRead(mappingFilePath);
+        int index=0;
+        while (fp.hasNextLine())
+        {
+            String partOfSpeech=fp.readNextLine();
+            mapPartOfSpeech.put(partOfSpeech,index++);
+        }
+        fp.fileClose();
+        String ruleWords[]=rule.split(" ");
+        int ruleArray[]=new int[ruleWords.length];
+        int ruleLen=0;
+        for (ruleLen=0;ruleLen<ruleWords.length ;ruleLen++ )
+        {
+            if(mapPartOfSpeech.containsKey(ruleWords[ruleLen]))
+                ruleArray[ruleLen]=mapPartOfSpeech.get(ruleWords[ruleLen]);
+        }
+        for (int i=0;i < length ;i++ )
+        {
+            ruleWords=sourceRules[i].split(" ");
+            int ruleSourceArray[]=new int[ruleWords.length];
+            int sourceRuleLen=0;
+            for (sourceRuleLen=0;sourceRuleLen<ruleWords.length ;sourceRuleLen++ )
+                ruleSourceArray[sourceRuleLen]=mapPartOfSpeech.get(ruleWords[sourceRuleLen]);
+            arr[i]=algo.editDistance(ruleArray,ruleSourceArray);
+        }
+        int min=UtilityClass.findMin(length,arr);
+        matchingSourceRules = new String[maxRules];
+        matchingTargetRules = new String[maxRules];
+        matchLength=0;
+        for (int i=0;i < length ; i++ )
+        {
+            if (min==arr[i])
+            {
+                matchingTargetRules[matchLength]=targetRules[i];
+                matchingSourceRules[matchLength++]=sourceRules[i];
+            }
+        }
+    }
 }
