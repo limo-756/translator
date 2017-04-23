2124e93227273b13e02dcbb596fc3f01c1822bd6
limo-756 <anuragsharma05031996@gmail.com>
2017-04-24 00:17:39 +0530

    Add sentenceparser file this will be used to set and retrive properties from sentence.

---
+++ sentenceParser.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/**
+ *
+ * it parses hindi sentence and sets the properties of the sentence
+ * it also parse translated english sentence and enforce the rules on that sentence
+ */
+class Parser
+{
+    String specificSyntaxFilePath="/media/anurag/New Volume/translator/project/Syntax";
+    String propertyFilePath="/media/anurag/New Volume/translator/project/property";
+    String valueFilePath="/media/anurag/New Volume/translator/project/value";
+    String getFilePath="/media/anurag/New Volume/translator/project/get";
+    HashMap<String,Integer> propMap=new HashMap<String,Integer>();
+    HashMap<String,Integer> valueMap=new HashMap<String,Integer>();
+    ArrayList bugyArray=new ArrayList();
+    HashMap<String,String> map;
+    int maxLineLimit=200;
+    int len=0;
+    String lineArray[]=new String[maxLineLimit];
+    String property[]=new String[100];
+    String value[]=new String[100];
+    /**
+     * It reads the property and values from provided files and provides
+     * the index to indivisual property using hashmap function
+     */
+    Parser()
+    {
+        FileRead fp=new FileRead(propertyFilePath);
+        int index=1;
+        while (fp.hasNextLine())
+        {
+            String temp=fp.readNextLine();
+            propMap.put(temp,index++);
+        }
+        fp.fileClose();
+        fp=new FileRead(valueFilePath);
+        index=1;
+        while (fp.hasNextLine())
+        {
+            String temp=fp.readNextLine();
+            value[index]=temp;
+            valueMap.put(temp,index++);
+        }
+        fp.fileClose();
+    }
+}
